368a39410007462b3f44634fb00a92598568c090
608206 <bryanmusungu@C02ZD0V8LVDT.local>
2020-03-18 09:56:40 -0400

    first commit

---
+++ tutorial/tutorial/quotes_spider.py
@@ -0,0 +1,13 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'title'
+    start_urls = [
+        'https://www.goal.com/en-us'
+    ]
+
+    def parse(self, response):
+        for quote in response.xpath('//h1[@class="page-header"]').get():
+            yield {"title": quote}
+            
+
